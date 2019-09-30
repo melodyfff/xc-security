40612e4486c0aa5a293419069a5bdc97bbe36874
melodyfff <307208327@qq.com>
2019-09-30 17:22:18 +0800

    :smile: init

---
+++ src/main/java/com/xinchen/security/App.java
@@ -0,0 +1,26 @@
+package com.xinchen.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ *
+ * 也可以继承{@link SpringBootServletInitializer}
+ *
+ * <pre>
+ *     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+ *         return builder.sources(App.class);
+ *     }
+ * </pre>
+ *
+ * @author xinchen
+ * @version 1.0
+ * @date 30/09/2019 14:07
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
---
+++ src/main/java/com/xinchen/security/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.xinchen.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+/**
+ * @author xinchen
+ * @version 1.0
+ * @date 30/09/2019 14:26
+ */
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user = User.builder().username("hello")
+                .password("hello")
+                .roles("USER")
+                .passwordEncoder((password)-> passwordEncoder().encode(password))
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public static BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(4);
+    }
+}
